a9f72b2d5a6b3aa7bc917da59486d2b217564561
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-04 21:39:57 +0545

    push

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const http = require('http');
 
 
 const Server = http.createServer((req, res) =>{
-    console.log(req);
-    process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete Coding</title></head>')
+    res.write('<body><h1>Like /Share /Subscribe</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 const PORT = 3000;
