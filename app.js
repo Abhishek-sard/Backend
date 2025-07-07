25179df871a697bd202ec73a2f788c371280722e
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-07 10:11:46 +0545

    push

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const http = require('http');
+const http = require("http");
 
+const Server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
 
-
-const Server = http.createServer((req, res) =>{
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Complete Coding</title></head>')
-    res.write('<body><h1>Like /Share /Subscribe</h1></body>');
-    res.write('</html>');
-    res.end();
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({ message: "This is the home page" }));
+    return res.end();
+  } else if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body><h1>This is about paged</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Complete Coding</title></head>");
+  res.write("<body><h1>Like /Share /Subscribe</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 3000;
 Server.listen(PORT, () => {
-    console.log(`Server is running on address http://localhost: ${PORT}`);
-}) 
+  console.log(`Server is running on address http://localhost: ${PORT}`);
+});
