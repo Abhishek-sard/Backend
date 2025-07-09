d7177159bb5f0ac6d0221900269aa38c58d220e8
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-09 21:13:02 +0545

    push

--- PraticeSet/Pratice/handler.js
+++ PraticeSet/Pratice/handler.js
@@ -1,7 +1,7 @@
 const requestHandler = (req, res) => {
   console.log(req.url, req.method);
 
-  if (req.url === '/') {
+  if (req.url === '/home') {
     res.setHeader('Content-Type', 'text/html');
     res.write(`
       <html>
@@ -9,13 +9,45 @@ const requestHandler = (req, res) => {
           <title>Pratice Set</title>
         </head>
         <body>
-          <h1>404 is not exist</h1>
+          <h1>Welcome to Calculator</h1>
           <a href="/Calculator">Go to Calculator</a>
         </body>
       </html>
     `);
     return res.end();
+  }else if (req.url.toLowerCase() === '/Calculator') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+      <html>
+        <head>
+          <title>Calculator</title>
+        </head>
+        <body>
+          <h1>Here is the Calculator</h1>
+          <form action="/calculate" method="POST">
+            <input type="text" name="expression" placeholder="Enter expression" />
+            <button type="submit">Calculate</button>
+          </form>
+        </body>
+      </html>
+    `);
+    return res.end();
   }
+  res.setHeader('Content-Type', 'text/html');
+  res.write(
+    `
+      <html>
+        <head>
+          <title>Pratice Set</title>
+        </head>
+        <body>
+          <h1>Welcome to Pratice Set</h1>
+          <a href="/home">Go to Home</a>
+        </body>
+      </html>
+    `);
+  return res.end();
+  
 };
 
 module.exports = requestHandler;
