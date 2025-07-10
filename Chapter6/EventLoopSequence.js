a15db863f32da5676ce5d53b3806b6b23294582e
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-10 11:24:45 +0545

    go

---
+++ Chapter6/EventLoopSequence.js
@@ -0,0 +1,26 @@
+console.log('1. Start of script');
+
+//Microtask queue (promise)
+Promise.resolve().then {() =>
+    console.log('2. Microtask');
+}
+
+//Timer queue
+setTimeout(() => 
+    console.log('3. Timer 1 '), 0);
+
+
+//I/O queue
+const fs = require('fs');
+fs.readFile('user-details.txt', () => console.log('4. I/O operation'));
+
+//Check queue
+setImmediate(() => 
+console.log('5. Immediate callback'));
+
+//Close queue
+process.on('exit', () => {
+    console.log(' 6. exit event')
+});
+
+console.log('7. End of script');
