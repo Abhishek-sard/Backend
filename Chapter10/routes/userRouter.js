1da61765700bc2b889ff56bafbae565ba859fb2a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 15:01:49 +0545

    push

--- Chapter10/routes/userRouter.js
+++ Chapter10/routes/userRouter.js
@@ -4,8 +4,11 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();  // create router instance
 
+//local modules
+const rootDir = require("../utils/pathUtil");
+
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/home.html'));
+    res.sendFile(path.join(rootDir, '../views/home.html'));
 });
 
 module.exports = router;  // export router instance
