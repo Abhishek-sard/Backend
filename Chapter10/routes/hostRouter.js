9c4905c39f4b4321afc82f36e8cdaf259583bd89
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 15:06:24 +0545

    go

--- Chapter10/routes/hostRouter.js
+++ Chapter10/routes/hostRouter.js
@@ -4,12 +4,15 @@ const path = require('path');
 const express = require('express');
 const hostRouter = express.Router();
 
+//local modules
+const rootDir = require("../utils/pathUtil");
+
 hostRouter.get("/add-home", (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/addhome.html'));
+  res.sendFile(path.join(rootDir, '../views/addhome.html'));
 });
 
 hostRouter.post("/add-home", (req, res) => {
-res.sendFile(path.join(__dirname, '../views/Homeadded.html'));
+res.sendFile(path.join(rootDir, '../views/Homeadded.html'));
   res.send(`<h1>Home registered successfully</h1>
       <a href="/">Go to Home</a>`);
 });
