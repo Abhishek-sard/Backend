959d17850d02c118bfe80eb2c1564b87f9512c4a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 06:39:47 +0545

    push

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,27 @@
+//core module
 const http = require("http");
+
+//External module
+const express = require('express');
+
+//local module
 const requestHandler = require("./requestHandler");
+const app = express();
 
-const Server = http.createServer(requestHandler); 
+app.use((req,res,next)=>{
+  console.log("Came in first middleware");
+  next();
+});
 
+
+app.use((req,res,next)=>{
+  console.log("Came in second middleware");
+  next();
+})
 const PORT = 3000;
-Server.listen(PORT, () => {
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
   console.log(`Server is running on address http://localhost:${PORT}`);
 });
