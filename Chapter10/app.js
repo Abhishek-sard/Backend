fcf78510cd4ecc1ea176f7c41b4e7de145ad80e9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 14:19:26 +0545

    go

--- Chapter10/app.js
+++ Chapter10/app.js
@@ -7,11 +7,14 @@ const hostRouter = require("./routes/hostRouter");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-// ✅ Correct router usage
+app.use(express.urlencoded());
 app.use(userRouter);
-app.use("/host",hostRouter);
+app.use(hostRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Your page is not found in aribnb</h1>")
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- Chapter10/routes/hostRouter.js
+++ Chapter10/routes/hostRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hostRouter = express.Router();
 
-hostRouter.get("/add-home", (req, res) => {
+hostRouter.get("/host/add-home", (req, res) => {
   res.send(`<h1>Register Your Home Now</h1>
       <form action="/host/add-home" method="POST">
           <input type="text" name="homeName" placeholder="Home Name" required>
@@ -9,7 +9,7 @@ hostRouter.get("/add-home", (req, res) => {
       </form>`);
 });
 
-hostRouter.post("/add-home", (req, res) => {
+hostRouter.post("/host/add-home", (req, res) => {
   console.log(req.body);
   res.send(`<h1>Home registered successfully</h1>
       <a href="/">Go to Home</a>`);
