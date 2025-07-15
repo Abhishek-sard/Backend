f856b3e53bcf063bfd144b7c8cd4f1dbd6694d07
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 07:08:05 +0545

    push

--- Express/app.js
+++ Express/app.js
@@ -5,9 +5,10 @@ const http = require("http");
 const express = require('express');
 
 //local module
-const requestHandler = require("./requestHandler");
 const app = express();
 
+//to route the users.js
+const users = require('./users');
 app.use((req,res,next)=>{
   console.log("Came in first middleware");
   next();
@@ -15,7 +16,8 @@ app.use((req,res,next)=>{
 
 
 app.use((req,res,next)=>{
-  console.log("Came in second middleware");
+  console.log("Came in second middleware", req.url, req.method);
+  res.send('<h1>Welcome to Express App</h1>');
   next();
 })
 const PORT = 3000;
---
+++ Express/users.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const fs = require("fs");
+
+const Server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body>");
+    res.write("<h1>Enter Your Details:</h1>");
+    res.write('<form method="POST" action="/submit-details">');
+    res.write('<input type="text" name="name" placeholder="Name" required><br><br>');
+    res.write('<input type="email" name="email" placeholder="Email" required><br><br>');
+    res.write('<input type="radio" id="male" name="gender" value="male" required>');
+    res.write('<label for="male">Male</label><br>');
+    res.write('<input type="radio" id="female" name="gender" value="female">');
+    res.write('<label for="female">Female</label><br><br>');
+    res.write('<button type="submit">Submit</button>');
+    res.write('</form>');
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }else if (req.url.toLowerCase() === "/submit-details" && req.method == "POST" ){
+
+    req.on('data', chunk =>{
+      console.log(chunk);
+    });
+    {
+      fs.writeFileSync('user.txt', 'Abhishek Sardar');
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+    }
+  }
+
+  // Handle other routes
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Complete Coding</title></head>");
+  res.write("<body><h1>Like / Share / Subscribe</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+const PORT = 3000;
+Server.listen(PORT, () => {
+  console.log(`Server is running on address http://localhost:${PORT}`);
+});
