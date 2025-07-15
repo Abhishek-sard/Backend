c57cf72bbd29980e274f44397a318283e4a2ae9d
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-15 11:14:36 +0545

    go

--- Chapter10/app.js
+++ Chapter10/app.js
@@ -1,24 +1,27 @@
 //External Modules
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
-
-app.get("/",(req, res, next) => {
-    console.log(req.url, req.method);
-    res.send(`<h1>Welcome to airbhub </h1> 
+app.get("/", (req, res, next) => {
+  console.log(req.url, req.method);
+  res.send(`<h1>Welcome to airbhub </h1> 
         <a href="/add-home">Add Home</a>`);
 });
 app.get("/add-home", (req, res, next) => {
-  res.send(`<h1>Add Home</h1>
+  res.send(`<h1>Register Your Home Now</h1>
       <form action="/add-home" method="POST">
           <input type="text" name="homeName" placeholder="Home Name" required>
           <button type="submit">Add Home</button>
       </form>`);
 });
 
+app.post("/add-home", (req, res, next) => {
+  res.send(`<h1>Home Register sucessfully</h1>
+      <a href="/">Go to Home</a>`);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on address http://localhost:${PORT}`);
+  console.log(`Server is running on address http://localhost:${PORT}`);
 });
