4862af69956138f5a3b1b3c3c1dcf431717c6f06
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-16 07:27:45 +0545

    po

--- Chapter11-Styling/routes/hostRouter.js
+++ Chapter11-Styling/routes/hostRouter.js
@@ -8,11 +8,11 @@ const hostRouter = express.Router();
 const rootDir = require("../utils/pathUtil");
 
 hostRouter.get("/add-home", (req, res) => {
-  res.sendFile(path.join(rootDir, '../views/addhome.html'));
+  res.sendFile(path.join(rootDir, '../views/addHome.html'));
 });
 
 hostRouter.post("/add-home", (req, res) => {
-res.sendFile(path.join(rootDir, '../views/Homeadded.html'));
+  res.sendFile(path.join(rootDir, '../views/HomeAdded.html'));
   res.send(`<h1>Home registered successfully</h1>
       <a href="/">Go to Home</a>`);
 });
