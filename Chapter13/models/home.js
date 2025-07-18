88277942fd7d27e00e86f6cb95cbe6eeba5a559b
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-18 14:28:23 +0545

    push

--- Chapter13/models/home.js
+++ Chapter13/models/home.js
@@ -1,18 +1,31 @@
-//fake database
-const registeredHomes = [];
+// Core Modules
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
 
 module.exports = class Home {
-constructor(houseName, price, location, rating, photoUrl){
+  constructor(houseName, price, location, rating, photoUrl) {
     this.houseName = houseName;
     this.price = price;
     this.location = location;
     this.rating = rating;
     this.photoUrl = photoUrl;
-}
-save() {
-    registeredHomes.push(this);
-}
-static fetchAll() {
-    return registeredHomes;
-}
+  }
+
+  save() {
+    Home.fetchAll((registeredHomes) => {
+      registeredHomes.push(this);
+      const homeDataPath = path.join(rootDir, "data", "homes.json");
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+        console.log("File Writing Concluded", error);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    const homeDataPath = path.join(rootDir, "data", "homes.json");
+    fs.readFile(homeDataPath, (err, data) => {
+      callback(!err ? JSON.parse(data) : []);
+    });
+  }
 };
